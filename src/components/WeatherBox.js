9b2d462f999eaf4b462e1fd1aebb84d19a4f040a
ryu14110 <119416694+ryu14110@users.noreply.github.com>
2023-08-04 07:05:56 +0900

    Update WeatherBox.js

--- src/components/WeatherBox.js
+++ src/components/WeatherBox.js
@@ -7,7 +7,7 @@ function WeatherBox({weather}) {
     <div className='weather-box'>
       {/* <div>{weather && weather.name}</div> */}
       <div className='city-color'><h3>{weather?.name}</h3></div>
-      <h1>{Math.floor(weather?.main.temp)}C / {Math.floor((weather?.main.temp* 9)/5+32)}F</h1>
+      <h1>{Math.floor(weather?.main.temp)} C / {Math.floor((weather?.main.temp* 9)/5+32)} F</h1>
       <h2 className='des-color'>{weather?.weather[0].description}</h2>
       <h3>Humidity : {weather?.main.humidity}%</h3>
     </div>
